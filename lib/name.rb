d9b56299f7ba1f3f3c50e95d97bfbf8e97faf89f
Amir Khan <ak2022dev@gmail.com>
2022-08-02 15:29:40 +0100

    Test-drive name class first name initialisation test
    Simple test to ensure default first name is empty if no argument
    given to constructor

--- lib/name.rb
+++ lib/name.rb
@@ -1,5 +1,12 @@
 # File: name.rb
 
 class Name
+
+  attr_reader :first
+
+  def initialize
+    @first = ""
+  end
+
 end
 
--- spec/name_spec.rb
+++ spec/name_spec.rb
@@ -3,5 +3,11 @@
 require 'name'
 
 RSpec.describe Name do
+  context "It initialises with blank values by default" do
+    it "Initialises with a blank first name" do
+      name = Name.new
+      expect(name.first).to eq ""
+    end
+  end
 end
 
