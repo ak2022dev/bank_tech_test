d34aab731716e6e2df80623b7a593362585062d2
Amir Khan <ak2022dev@gmail.com>
2022-08-02 16:07:38 +0100

    Refactor name class constructor with default values
    Added default values to name class constructor. All are set to "".

--- lib/name.rb
+++ lib/name.rb
@@ -4,10 +4,10 @@ class Name
 
   attr_reader :first, :middle, :last
 
-  def initialize
-    @first = ""
-    @middle = ""
-    @last = ""
+  def initialize( first="", middle="", last="" )
+    @first = first
+    @middle = middle
+    @last = last
   end
 
 end
