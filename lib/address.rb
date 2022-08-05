5f966604c941e66135bee5dda95ac8da20de3025
Amir Khan <ak2022dev@gmail.com>
2022-08-05 14:51:08 +0100

    TDD initialize method for address class
    Check second address field can default to blank as expected

--- lib/address.rb
+++ lib/address.rb
@@ -1,5 +1,15 @@
 # File: address.rb
 
 class Address
+
+  attr_reader :address1, :address2, :district, :postcode
+
+  def initialize( add1, add2 = "", district, postcode )
+    @address1 = add1
+    @address2 = add2
+    @district = district
+    @postcode = postcode  
+  end
+
 end
 
--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -3,7 +3,12 @@
 require 'address'
 
 RSpec.describe Address do
-  context "It initialises with blank values by default" do
+  context "It initialises with blank values when appropriate" do
+    it "has a blank address_2 field by default" do
+      address = Address.new("First line", "District", "")
+      address2 = address.address2
+      expect(address2).to eq ""
+    end
   end
 end
 
