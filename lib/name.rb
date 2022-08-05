84f1ea207db32387c9e73d8014833005e4c04b04
Amir Khan <ak2022dev@gmail.com>
2022-08-05 10:31:27 +0100

    Refactor add assignStrippedNames method to name class

--- lib/name.rb
+++ lib/name.rb
@@ -2,14 +2,15 @@
 
 class Name
 
+
   attr_reader :first, :middle, :last
+
 
   def initialize( first, middle="", last="" )
     checkNamePresence( first, middle, last );
-    @first = first.strip
-    @middle = middle.strip
-    @last = last.strip
+    assignStrippedNames( first, middle, last );
   end
+
 
   def checkNamePresence( first, middle, last )
     if (first == "" || first == nil )
@@ -22,15 +23,25 @@ class Name
     end
   end 
   
+
+  def assignStrippedNames( first, middle, last )
+    @first = first.strip
+    @middle = middle.strip
+    @last = last.strip
+  end
+
+
   def self.valid_first_name?( name )
     # Allows names like "Mary-Sue"
     ((name =~ /^[A-Z][a-z]*(-[A-z][a-z]*)*$/) == 0) ? true : false 
   end
 
+
   def self.valid_last_name?( name )
     # Currently rules for last name same as for first name
     self.valid_first_name?(name)
   end
+
 
   def self.valid_mid_name?( name )
     # Allows names like "Patrick Smith-Jones"
