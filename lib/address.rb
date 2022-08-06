8386586e07e182a409eefd413c4de50b24b3c8b8
Amir Khan <ak2022dev@gmail.com>
2022-08-06 10:19:56 +0100

    Add tests to check address class correctly stores fields on construction

--- lib/address.rb
+++ lib/address.rb
@@ -3,7 +3,6 @@
 class Address
 
   attr_reader :address1, :address2, :district, :postcode
-
   def initialize( add1, add2 = "", district, postcode )
     @address1 = add1
     @address2 = add2
--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -10,6 +10,28 @@ RSpec.describe Address do
       expect(address2).to eq ""
     end
   end
+  context "It initialises with arguments passed to it" do
+    it "remembers the first line of the address" do
+      address = Address.new("First line", "District", "")
+      add1 = address.address1
+      expect(add1).to eq "First line"
+    end  
+    it "remembers the district of the address" do
+      address = Address.new("First line", "District", "")
+      dist = address.district
+      expect(dist).to eq "District"
+    end  
+    it "remembers the second line of the address" do
+      address = Address.new("First line", "Second line", "District", "")
+      add2 = address.address2
+      expect(add2).to eq "Second line"
+    end  
+    it "remembers the postcode of the address" do
+      address = Address.new("First line", "Second line", "District", "PC5 9DC")
+      add2 = address.postcode
+      expect(add2).to eq "PC5 9DC"
+    end  
+  end
 end
 
 
