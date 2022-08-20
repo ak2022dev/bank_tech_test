f1264fe1fa1f8cc93bb6d4f1c0a1065204f1a0ff
Amir Khan <ak2022dev@gmail.com>
2022-08-20 09:32:35 +0100

    Begin TDD of CustomerIdNIStrategy class
    This is a concrete implementation subclass of CustomerIdStrategy
    abstract class.

---
+++ lib/customer_id_ni_strategy.rb
@@ -0,0 +1,7 @@
+# File: customer_id_ni_strategy.rb
+
+require 'customer_id_strategy'
+
+class CustomerIdNIStrategy < CustomerIdStrategy
+
+end
---
+++ spec/customer_id_ni_strategy_spec.rb
@@ -0,0 +1,10 @@
+# File: customer_id_ni_strategy_spec.rb
+
+require 'customer_id_ni_strategy'
+
+RSpec.describe CustomerIdNIStrategy do
+  it "Has a provide_id method" do
+    strat = CustomerIdNIStrategy.new
+    expect( strat ).to respond_to( :provide_id )
+  end
+end
