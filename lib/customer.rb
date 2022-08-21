4a76c4be40614b9a637cc11c69eb62f3591888c3
Amir Khan <ak2022dev@gmail.com>
2022-08-21 16:08:35 +0100

    TDD add name field to Customer class using Name class

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,14 +2,21 @@
 
 require 'customer_id_strategy'
 require 'customer_id_ni_strategy'
+require 'name'
 
 class Customer
 
-  attr_reader :id
+  attr_reader :id, :name
 
-  def initialize
-    # @id = ""
+  def initialize( first_name, middle_name = "", last_name = "" )
     @id = CustomerIdNIStrategy.new.provide_id
+    set_name( first_name, middle_name, last_name )
+  end
+
+  private 
+
+  def set_name( first_name, middle_name, last_name )
+    @name = Name.new( first_name, middle_name, last_name )
   end
 
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -5,8 +5,16 @@ require 'customer'
 RSpec.describe Customer do
   context "It uses the strategy pattern to provide a unique id" do
     it "has a non-nil id" do
-      cust = Customer.new
+      cust = Customer.new( "First" )
       expect( cust.id ).not_to be_nil
+    end 
+  end
+  context "It uses a name object to store a valid name" do
+    it "remembers the customer's name using a name object" do
+      cust = Customer.new( "First", "Middle", "Last" )
+      expect( cust.name.first ).to eq "First"
+      expect( cust.name.middle ).to eq "Middle"
+      expect( cust.name.last ).to eq "Last"
     end 
   end
 end
