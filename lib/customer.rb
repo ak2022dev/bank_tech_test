7bcd24afdd9d31eeac9281d7902e643173111b1f
Amir Khan <ak2022dev@gmail.com>
2022-08-21 18:15:17 +0100

    Refactor Customer class added set_id method for consistency in setup of instance variables

--- lib/customer.rb
+++ lib/customer.rb
@@ -10,12 +10,16 @@ class Customer
   attr_reader :id, :name, :dob
 
   def initialize( first_name, middle_name = "", last_name = "", dob_year, dob_month, dob_day )
-    @id = CustomerIdNIStrategy.new.provide_id
+    set_id
     set_name( first_name, middle_name, last_name )
     set_dob( dob_year, dob_month, dob_day )
   end
 
   private 
+
+  def set_id
+    @id = CustomerIdNIStrategy.new.provide_id
+  end
 
   def set_name( first_name, middle_name, last_name )
     @name = Name.new( first_name, middle_name, last_name )
