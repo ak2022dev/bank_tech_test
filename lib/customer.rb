2e24f14998fbbbac414bfe5464daad2ca3e6770e
Amir Khan <ak2022dev@gmail.com>
2022-08-21 15:43:18 +0100

    Refactor Customer class to obtain id using Strategy design pattern
    Uses the CustomerIdNIStrategy concrete class implementation of
    the CustomerIdStrategy abstract class, which were built using
    TDD and unit tested.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,15 @@
 # File: customer.rb
 
+require 'customer_id_strategy'
+require 'customer_id_ni_strategy'
+
 class Customer
 
   attr_reader :id
 
   def initialize
-    @id = ""
+    # @id = ""
+    @id = CustomerIdNIStrategy.new.provide_id
   end
+
 end
