d5f7a9751575f1d2378bc3d2b0ea436b1a5a9416
Amir Khan <ak2022dev@gmail.com>
2022-08-21 18:11:57 +0100

    TDD add dob field to Customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -3,20 +3,26 @@
 require 'customer_id_strategy'
 require 'customer_id_ni_strategy'
 require 'name'
+require 'date'
 
 class Customer
 
-  attr_reader :id, :name
+  attr_reader :id, :name, :dob
 
-  def initialize( first_name, middle_name = "", last_name = "" )
+  def initialize( first_name, middle_name = "", last_name = "", dob_year, dob_month, dob_day )
     @id = CustomerIdNIStrategy.new.provide_id
     set_name( first_name, middle_name, last_name )
+    set_dob( dob_year, dob_month, dob_day )
   end
 
   private 
 
   def set_name( first_name, middle_name, last_name )
     @name = Name.new( first_name, middle_name, last_name )
+  end
+
+  def set_dob( dob_year, dob_month, dob_day )
+    @dob = Date.new( dob_year, dob_month, dob_day )
   end
 
 end
