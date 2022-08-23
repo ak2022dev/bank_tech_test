4de9695a9ac0c53f115b3c4c6e089bca4657b00a
Amir Khan <ak2022dev@gmail.com>
2022-08-23 16:45:34 +0100

    Begin TDD of TransactionCommand
    Created and tested abstract Transaction class for use with the
    Command design pattern.

---
+++ lib/transaction_command.rb
@@ -0,0 +1,10 @@
+# File: transaction_command.rb
+
+class TransactionCommand
+
+    def transact( balance_in )
+      # Will return the new balance
+      # after the transaction has been completed
+    end
+
+end
