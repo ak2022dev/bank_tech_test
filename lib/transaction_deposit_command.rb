2d0636fa6c6ca5507e9da8515fbb19f7c124699b
Amir Khan <ak2022dev@gmail.com>
2022-08-24 15:10:50 +0100

    Refactor TransactionDepositCommand transact method to validate input

--- lib/transaction_deposit_command.rb
+++ lib/transaction_deposit_command.rb
@@ -5,8 +5,8 @@ require 'transaction_command'
 class TransactionDepositCommand < TransactionCommand
   def transact( balance_in, *args )
     # Will return the new balance
-    # after the deposit is made, expecting the deposit
-    # amount as args[0]
-    return balance_in + args[0]
+    # after the deposit is made
+    return balance_in + args[0] if args[0] > 0
+    raise ArgumentError.new( "Deposit needs to be greater than zero." )
   end  
 end
--- spec/transaction_deposit_command_spec.rb
+++ spec/transaction_deposit_command_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe TransactionDepositCommand do
     command = TransactionDepositCommand.new
     expect( command.transact( 1000, 200 ) ).to eq 1200
   end
+  it "Detects when an erroneous zero deposit is attempted" do
+    command = TransactionDepositCommand.new
+    expect{ command.transact( 1000, 0 ) }.to raise_error( ArgumentError, "Deposit needs to be greater than zero." )
+  end
+  it "Detects when an invalid negative deposit is attempted" do
+    command = TransactionDepositCommand.new
+    expect{ command.transact( 1000, -5 ) }.to raise_error( ArgumentError, "Deposit needs to be greater than zero." )
+  end
 end
