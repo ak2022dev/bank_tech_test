55901b59fb29585518d3a0b78ee9e27652ad8bcc
Amir Khan <ak2022dev@gmail.com>
2022-08-25 14:34:10 +0100

    TDD add account number to Account class

--- lib/account.rb
+++ lib/account.rb
@@ -4,10 +4,11 @@ require "customer"
 
 class Account
 
-  attr_reader :customer
+  attr_reader :customer, :account_number
   
-  def initialize( first_name, middle_name = "", last_name = "", dob_year, dob_month, dob_day,
+  def initialize( acc_num, first_name, middle_name = "", last_name = "", dob_year, dob_month, dob_day,
     address1, address2, district, postcode )
+    @account_number = acc_num   # This is actually a string
     @customer = Customer.new( first_name, middle_name, last_name, dob_year, dob_month, dob_day,
     address1, address2, district, postcode )
   end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,9 +3,16 @@
 require 'account'
 
 RSpec.describe Account do
+  context "It has an account number" do
+    it "Remembers its allocated account number" do
+        account = Account.new( "00000001", "First", 2020, 12, 31,
+        "Address1", "", "District", "A9 8BC" )
+        expect( account.account_number ).to eq "00000001"
+    end
+  end
   context "It uses composition to record its customer" do
     it "Belongs to a customer" do
-      account = Account.new( "First", 2020, 12, 31,
+      account = Account.new( "00000001", "First", 2020, 12, 31,
                              "Address1", "", "District", "A9 8BC" )
       expect( account.customer ).not_to be_nil
     end
